72ee9d5c78ac754978c01e588b695e9ffb39797d
Rachel Lin <rachel.lynch.lin@gmail.com>
2019-04-19 14:16:18 -0700

    Most buttons work now with smooth scrolling

--- js/main.js
+++ js/main.js
@@ -7,4 +7,47 @@ $(document).ready(function() {
       $('.navbar').removeClass('fixed-top');
     }
   }) 
-})
+});
+
+$(".navbar-dark ul li a[href^='#']").on('click', function(e) {
+  
+  e.preventDefault();
+
+  var hash = this.hash;
+
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top
+  }, 300, function(){
+    window.location.hash = hash;
+  });
+
+});
+
+$("#section-parallax .col-sm-8 a[href^='#']").on('click', function (e) {
+
+  e.preventDefault();
+
+  var hash = this.hash;
+
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top
+  }, 300, function () {
+    window.location.hash = hash;
+  });
+
+});
+
+
+$(".footer .scroll-to-top-button a[href^='#']").on('click', function (e) {
+
+  e.preventDefault();
+
+  var hash = this.hash;
+
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top
+  }, 300, function () {
+    window.location.hash = hash;
+  });
+
+});
